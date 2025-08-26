33727ee4a633d38d3d9dc0f93731ad9811e65496
Ashutosh Singh <hiashutoshkumarsingh@gmail.com>
2025-08-26 22:41:33 +0530

    Initial Commit

---
+++ khaikhai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  khaikhai
+//
+//  Created by Ashutosh Singh on 26/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
